9abcbcff8f0c310b18a36b644d1c3e8150acfca6
Alexey Solovyev <solovaa@yandex.ru>
2025-03-07 08:44:38 +0000

    add fixture for third product and 2 category

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 
+from src.category import Category
 from src.product import Product
 
 
@@ -12,3 +13,27 @@ def second_product():
     return Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
 
 
+@pytest.fixture
+def third_product():
+    return Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
+
+
+@pytest.fixture
+def first_category(first_product, second_product):
+    products = [first_product, second_product]
+    category = Category(
+        "Смартфоны",
+        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+        products
+    )
+    return category
+
+
+@pytest.fixture
+def second_category(third_product):
+    category = Category(
+        "Телевизоры",
+        "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
+        [third_product]
+    )
+    return category
