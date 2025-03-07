fadaa43ec97a876b540fa36d01e16def45ccc5ce
Alexey Solovyev <solovaa@yandex.ru>
2025-03-07 09:36:16 +0000

    fix by linters

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,10 +1,8 @@
 import os
-from unittest.mock import patch, mock_open
 
 from config import PATH_DATA
 from src.category import Category
 from src.utils import read_json
-from tests.conftest import first_category
 
 
 def test_read_json():
@@ -18,8 +16,5 @@ def test_read_json():
     assert categories[0].products[0].name == "Samsung Galaxy C23 Ultra"
     assert categories[1].name == "Телевизоры"
     assert categories[1].description == ("Современный телевизор, "
-                                  "который позволяет наслаждаться просмотром, станет вашим другом и помощником")
+                                         "который позволяет наслаждаться просмотром, станет вашим другом и помощником")
     assert categories[1].products[0].name == "55\" QLED 4K"
-
-
-
