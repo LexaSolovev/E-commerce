a5400fe682145cdfc96d1f406fd1bc8cfc06cef3
Alexey Solovyev <solovaa@yandex.ru>
2025-03-08 17:59:53 +0000

    update tests

--- tests/test_category.py
+++ tests/test_category.py
@@ -2,13 +2,17 @@ from src.category import Category
 
 
 def test_category_init1(first_category, first_product, second_product):
-    products = [first_product, second_product]
+    products = [first_product]
     assert first_category.name == "Смартфоны"
     assert first_category.description == ("Смартфоны, как средство не только коммуникации, "
                                           "но и получения дополнительных функций для удобства жизни")
     assert first_category.products == products
     assert Category.category_count == 1
+    assert Category.product_count == 1
+    first_category.add_product(second_product)
     assert Category.product_count == 2
+    assert first_category.products_str == ("Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт.\n"
+                                           "Iphone 15, 210000.0 руб. Остаток: 8 шт.\n")
 
 
 def test_category_init2(second_category, third_product):
@@ -19,3 +23,4 @@ def test_category_init2(second_category, third_product):
     assert second_category.products == products
     assert Category.category_count == 2
     assert Category.product_count == 3
+    assert second_category.products_str == "55\" QLED 4K, 123000.0 руб. Остаток: 7 шт.\n"
