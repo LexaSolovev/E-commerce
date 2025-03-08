e99feb5e1a7ebbc19fcf983613ccffe188c78821
Alexey Solovyev <solovaa@yandex.ru>
2025-03-08 18:07:24 +0000

    add tests for new_product

--- tests/test_product.py
+++ tests/test_product.py
@@ -1,3 +1,4 @@
+from src.product import Product
 
 
 def test_product_init1(first_product):
@@ -12,3 +13,17 @@ def test_product_init2(second_product):
     assert second_product.description == "512GB, Gray space"
     assert second_product.price == 210000.0
     assert second_product.quantity == 8
+
+def test_product_new_product():
+    product = Product.new_product(
+        {
+        "name": "Samsung Galaxy S23 Ultra",
+        "description": "256GB, Серый цвет, 200MP камера",
+        "price": 180000.0,
+        "quantity": 5
+      }
+    )
+    assert product.name == "Samsung Galaxy S23 Ultra"
+    assert product.description == "256GB, Серый цвет, 200MP камера"
+    assert product.price == 180000.0
+    assert product.quantity == 5
